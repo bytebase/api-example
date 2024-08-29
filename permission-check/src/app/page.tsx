d030cf18719b2f471ac036b6cdf7a0076aa67259
adela <a@bytebase.com>
2024-08-29 11:43:01 +0800

    fix

--- permission-check/src/app/page.tsx
+++ permission-check/src/app/page.tsx
@@ -5,20 +5,20 @@ export default async function Home() {
 
   const token = await generateToken();
 
-   /* Fetch all users&roles in workspace */
-   const allWorkspaceIam = await fetch(`${process.env.NEXT_PUBLIC_BB_HOST}/v1/workspaces/*:getIamPolicy`, {
+  /* Fetch all users&roles in workspace */
+  const allWorkspaceIam = await fetch(`${process.env.NEXT_PUBLIC_BB_HOST}/v1/workspaces/*:getIamPolicy`, {
+    method: "GET",
+    headers: {
+      "Authorization": 'Bearer '+ token
+    }});
+  const allWorkspaceIamData = await allWorkspaceIam.json();
+
+  const allGroups = await fetch(`${process.env.NEXT_PUBLIC_BB_HOST}/v1/groups`, {
     method: "GET",
     headers: {
       "Authorization": 'Bearer '+ token
   }});
-  
-  const allWorkspaceIamData = await allWorkspaceIam.json();
-    const allGroups = await fetch(`${process.env.NEXT_PUBLIC_BB_HOST}/v1/groups`, {
-      method: "GET",
-      headers: {
-        "Authorization": 'Bearer '+ token
-    }});
-    const allGroupsData = await allGroups.json();
+  const allGroupsData = await allGroups.json();
 
   /* Fetch all roles in workspace */
   const allRoles = await fetch(`${process.env.NEXT_PUBLIC_BB_HOST}/v1/roles`, {
@@ -39,12 +39,12 @@ export default async function Home() {
    const allDatabasePermissionsData = Array.from(allDatabasePermissions);
  
   /* Fetch all projects */
-  const allProjectRes = await fetch(`${process.env.NEXT_PUBLIC_BB_HOST}/v1/projects`, {
+  const allProjects = await fetch(`${process.env.NEXT_PUBLIC_BB_HOST}/v1/projects`, {
     method: "GET",
     headers: {
       "Authorization": 'Bearer '+ token
   }});
-  const allProjectData = await allProjectRes.json();
+  const allProjectData = await allProjects.json();
 
   return (
     <main className="flex flex-col w-full p-10 items-center">
