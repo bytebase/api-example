695e6002202291771e3fa9e245201cb3984aa0e2
adela <a@bytebase.com>
2024-09-04 17:14:46 +0800

    docs: halfway

--- permission-check/src/components/fetch-user-permission-form.tsx
+++ permission-check/src/components/fetch-user-permission-form.tsx
@@ -20,7 +20,9 @@ export default function FetchUserPermissionForm({ allUsers, allDatabasePermissio
     }, [user, permission]);
 
     const updateDatabasesWithPermission = async () => {
-     //   console.log("enter updateDatabasesWithPermission ===================")
+        if (!user || !permission) {
+            return;
+        }
 
         const newDatabasesWithPermission: Array<{project: string, databases: any[]}> = [];
 
@@ -36,7 +38,6 @@ export default function FetchUserPermissionForm({ allUsers, allDatabasePermissio
         setUserGroups(newUserGroups);
 
         if (userHasPermissionWorkspace.hasPermission) {
-          //  console.log("!!!!!!!!!!!!!User has permission for the whole workspace:",userHasPermissionWorkspace);
             for (const project of allProjects) {
                 const fetchedDatabases = await fetch(`/api/databases/${encodeURIComponent(project.name)}`, {
                     method: 'GET'
@@ -44,9 +45,7 @@ export default function FetchUserPermissionForm({ allUsers, allDatabasePermissio
                 const fetchedDatabasesData = await fetchedDatabases.json();
                 newDatabasesWithPermission.push({project: project.name, databases: fetchedDatabasesData.databases});
             }
-
         } else {
-           // console.log("User does not have permission for the whole workspace, let's check the project iam");
             for (const project of allProjects) {
                 const projectShort = project.name.split("/")[1];
                 const fetchedProjectIam = await fetch(`/api/projectiam/${encodeURIComponent(projectShort)}`, {
@@ -55,76 +54,120 @@ export default function FetchUserPermissionForm({ allUsers, allDatabasePermissio
                 const fetchedProjectIamData = await fetchedProjectIam.json();
     
                 const userHasPermissionProject = checkUserPermission(rolesWithPermission, fetchedProjectIamData.bindings, userGroups.length > 0, project.name);
-                if (userHasPermissionProject.hasPermission) { // we should check if the permission is for databases only
-                   // console.log("User has permission for the project:", project.name);
+                if (userHasPermissionProject.hasPermission) {
                     const fetchedDatabases = await fetch(`/api/databases/${encodeURIComponent(project.name)}`, {
                         method: 'GET'
                     });
                     const fetchedDatabasesData = await fetchedDatabases.json();
                     newDatabasesWithPermission.push({project: project.name, databases: fetchedDatabasesData.databases});
-                  
                 } else {
-                    if (userHasPermissionProject.onlyDatabase && userHasPermissionProject.onlyDatabase != '') {
-                     //   console.log("userHasPermissionProject.onlyDatabase=======================", {project: project.name, databases: [userHasPermissionProject.onlyDatabase]})
-                        newDatabasesWithPermission.push({project: project.name, databases: [{name: userHasPermissionProject.onlyDatabase}]});
+                    if (userHasPermissionProject.onlyDatabase && userHasPermissionProject.onlyDatabase.length > 0) {
+                        userHasPermissionProject.onlyDatabase.forEach((db, index) => {
+                            const extraCondition = userHasPermissionProject.extraConditions[index];
+                            if (extraCondition) {
+                                newDatabasesWithPermission.push({project: project.name, databases: [{name: db, extraConditionTime: extraCondition.expireTime, extraConditionIsExpired: extraCondition.isExpired}]});
+                            } else {
+                                newDatabasesWithPermission.push({project: project.name, databases: [{name: db}]});
+                            }
+                        });
                     }
                 }
             }
         }
 
+        console.log("newDatabasesWithPermission ================= ", newDatabasesWithPermission)
         setDatabasesWithPermission(newDatabasesWithPermission);
-    }
+    };
 
-    const checkUserPermission = (roles: any[], bindings: any[], hasGroups: boolean = false, theProject: string = ''): { hasPermission: boolean, onlyDatabase: string } => {
-    
-        for (const binding of bindings) {
-            if (!roles.some(role => role.name === binding.role)) continue;
-    
-            const memberMatch = binding.members.includes(`user:${user}`);
+    // Group databases by project
+    const groupedDatabases = databasesWithPermission.reduce((acc, item) => {
+        if (!acc[item.project]) {
+            acc[item.project] = [];
+        }
+        acc[item.project].push(...item.databases);
+        return acc;
+    }, {});
+
+    const checkUserPermission = (rolesWithPermission: any[], rolesToBeMatched: any[], hasGroups: boolean = false, theProject: string = ''): { hasPermission: boolean, onlyDatabase: string[], extraConditions: {}[] } => {
+        let hasPermission = false;
+        let onlyDatabases: string[] = [];
+        let extraConditions: {}[] = [];
+
+        console.log("rolesWithPermission ================= ", rolesWithPermission)
+        console.log("rolesToBeMatched ================= ", rolesToBeMatched)
+        console.log("hasGroups ================= ", hasGroups)
+        console.log("theProject ================= ", theProject)
+
+        for (const roleToBeMatched of rolesToBeMatched) {
+            if (!rolesWithPermission.some(roleWithPermission => roleWithPermission.name === roleToBeMatched.role)) continue;
+
+            const memberMatch = roleToBeMatched.members.includes(`user:${user}`);
             const groupMatch = hasGroups && userGroups.some(group => 
-                binding.members.includes(group.replace('groups/', 'group:'))
+                roleToBeMatched.members.includes(group.replace('groups/', 'group:'))
             );
-    
+
             if (memberMatch || groupMatch) {
                 if (theProject !== '') {
-                    return checkProjectSpecificPermission(binding);
+                    const projectPermission = checkProjectSpecificPermission(roleToBeMatched);
+                    if (projectPermission.hasPermission) {
+                        hasPermission = true;
+                    }
+                    if (projectPermission.onlyDatabase) {
+                        onlyDatabases.push(projectPermission.onlyDatabase);
+                    }
+                    if (projectPermission.extraCondition) {
+                        extraConditions.push(projectPermission.extraCondition);
+                    }
+                } else {
+                    hasPermission = true;
                 }
-                return { hasPermission: true, onlyDatabase: '' };
             }
         }
-    
-        return { hasPermission: false, onlyDatabase: '' };
+
+        return { hasPermission, onlyDatabase: onlyDatabases, extraConditions };
     };
-    
-    const checkProjectSpecificPermission = (binding: any): { hasPermission: boolean, onlyDatabase: string } => {
-        if (!binding.condition || !binding.condition.expression) {
-            return { hasPermission: true, onlyDatabase: '' };
+
+    const checkProjectSpecificPermission = (roleToBeMatched: any): { hasPermission: boolean, onlyDatabase: string, extraCondition?: {} } => {
+        if (!roleToBeMatched.condition || !roleToBeMatched.condition.expression) {
+            return { hasPermission: true, onlyDatabase: '', extraCondition: {} };
         }
-    
-        const { expression } = binding.condition;
+
+        const { expression } = roleToBeMatched.condition;
+        console.log("expression ================= ", expression)
         const databaseMatch = expression.match(/resource\.database\s+in\s+\["([^"]+)"\]/);
         const timestampMatch = expression.match(/request\.time\s*<\s*timestamp\("([^"]+)"\)/);
-    
+
         if (!databaseMatch) {
-            return { hasPermission: true, onlyDatabase: '' };
+            return { hasPermission: true, onlyDatabase: '', extraCondition: {} };
         }
-    
+
+        let extraCondition: { isExpired?: boolean, expireTime?: string } = { isExpired: false, expireTime: '' };
+        let expirationTime: Date | null = null;
+
         if (timestampMatch) {
-            const expirationTime = new Date(timestampMatch[1]);
+            expirationTime = new Date(timestampMatch[1]);
             if (new Date() >= expirationTime) {
-                return { hasPermission: false, onlyDatabase: '' };
+                extraCondition = { isExpired: true, expireTime: expirationTime.toString() };
+            } else {
+                extraCondition = { isExpired: false, expireTime: expirationTime.toString() };
             }
         }
-    
-        return { hasPermission: false, onlyDatabase: databaseMatch[1] };
+
+        return { hasPermission: false, onlyDatabase: databaseMatch[1], extraCondition };
     };
 
+    // Sort databasesWithPermission before rendering
+    const sortedDatabasesWithPermission = databasesWithPermission.sort((a, b) => {
+        if (a.project < b.project) return -1;
+        if (a.project > b.project) return 1;
+        return 0;
+    });
 
     return (
-        <form onSubmit={handleSubmit}  className="md:w-1/2 sm:w-full flex gap-3 flex-col p-10 border-green-600 border-4">        
-        <h1 className="text-2xl font-bold">Which databases does a specific user have access to?</h1>  
+        <form onSubmit={handleSubmit} className="md:w-1/2 sm:w-full flex gap-3 flex-col p-10 border-green-600 border-4">        
+            <h1 className="text-2xl font-bold">Which databases does a specific user have access to?</h1>  
 
-        <select 
+            <select 
                 className="p-2 border rounded"
                 value={user}
                 onChange={(e) => handleSelectUser(e)}
@@ -135,43 +178,42 @@ export default function FetchUserPermissionForm({ allUsers, allDatabasePermissio
                 ))}
             </select>
 
-
-        {userGroups.length > 0 && (
-            <div>
-                <strong>The user also belongs to groups:</strong>
-                <ul>
-                {userGroups.map((group, index) => {
-                    return (
-                        <li key={index}>{group}</li>
-                    )
-                })}
-                </ul>
-            </div>
-        )}
-
-        <select name="permission" id="permission" value={permission} onChange={(e) => handleSelectPermission(e)}
-        className="w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
-        <option value="">-- Please select a permission --</option>
-        {allDatabasePermissions.map((itemadp, indexadp) => {
-            return  <option key={indexadp} value={itemadp}>{itemadp}</option>
-        })}
-        </select>
-
-        {databasesWithPermission.length > 0 && (
-        databasesWithPermission.map((item) => (
-            <div key={item.project} className="mt-4">
-            <h3 className="font-semibold text-lg">{item.project}</h3>
-            {item.databases.length > 0 && (
-                <ul className="list-disc pl-5 mt-2">
-                {item.databases.map((db) => (
-                    <li key={db.name}>{db.name}</li>
-                ))}
-                </ul>
-            ) }
-            </div>
-            ))
+            {userGroups.length > 0 && (
+                <div>
+                    <strong>The user also belongs to groups:</strong>
+                    <ul>
+                        {userGroups.map((group, index) => (
+                            <li key={index}>{group}</li>
+                        ))}
+                    </ul>
+                </div>
             )}
 
+            <select name="permission" id="permission" value={permission} onChange={(e) => handleSelectPermission(e)}
+                className="w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+                <option value="">-- Please select a permission --</option>
+                {allDatabasePermissions.map((itemadp, indexadp) => (
+                    <option key={indexadp} value={itemadp}>{itemadp}</option>
+                ))}
+            </select>
+
+            {Object.keys(groupedDatabases).length > 0 && (
+                Object.keys(groupedDatabases).map((project) => (
+                    <div key={project} className="mt-4">
+                        <h3 className="font-semibold text-lg">{project}</h3>
+                        <ul className="list-disc pl-5 mt-2">
+                            {groupedDatabases[project].map((db, index) => (
+                                <li 
+                                    key={`${db.name}-${index}`} 
+                                    style={{ textDecoration: db.extraConditionIsExpired ? 'line-through' : 'none' }}
+                                >
+                                    {db.name} {db.extraConditionTime}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))
+            )}
         </form>
     )
 }
