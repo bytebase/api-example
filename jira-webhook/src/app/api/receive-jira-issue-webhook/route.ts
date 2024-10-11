44bbe95d882961c57b57b68eef758a87fd47c3cf
adela <a@bytebase.com>
2024-10-11 16:00:12 +0800

    rename webhook

---
+++ jira-webhook/src/app/api/receive-jira-issue-webhook/route.ts
@@ -0,0 +1,4 @@
+// This file handles incoming Jira issue webhooks for both creation and updates
+import { generateToken, fetchData, createIssueWorkflow, updateJiraIssue } from '../utils';
+
+// ... (rest of the file remains the same)
